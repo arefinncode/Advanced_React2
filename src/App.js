1a41b9ee8c6b95235a081052f0ce72c406849ec5
arefinncode <arefin.nordecode@gmail.com>
2018-11-04 14:57:17 +0600

    nested_router_topics

--- src/App.js
+++ src/App.js
@@ -9,83 +9,83 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 const Index = () => <h2>Home</h2>;
 const About = () => <h2>About</h2>;
 const Users = () => <h2>Users</h2>;
-// const Home = () => <h2>Home</h2>;
-// const Topic = ({ match }) => <h3>Requested Param: {match.params.id}</h3>;
-// const Topics = ({ match }) => (
-//     <div>
-//         <h2>Topics</h2>
+const Home = () => <h2>Home</h2>;
+const Topic = ({ match }) => <h3>Requested Param: {match.params.id}</h3>;
+const Topics = ({ match }) => (
+    <div>
+        <h2>Topics</h2>
+
+        <ul>
+            <li>
+                <Link to={`${match.url}/components`}>Components</Link>
+            </li>
+            <li>
+                <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+            </li>
+        </ul>
+
+        <Route path={`${match.path}/:id`} component={Topic} />
+        <Route
+            exact
+            path={match.path}
+            render={() => <h3>Please select a topic.</h3>}
+        />
+    </div>
+);
+
+
+
+// const AppRouter = () => (
+//     <Router>
+//         <div>
+//             <nav>
+//                 <ul>
+//                     <li>
+//                         <Link to="/">Home</Link>
+//                     </li>
+//                     <li>
+//                         <Link to="/about/">About</Link>
+//                     </li>
+//                     <li>
+//                         <Link to="/users/">Users</Link>
+//                     </li>
+//                 </ul>
+//             </nav>
 //
-//         <ul>
-//             <li>
-//                 <Link to={`${match.url}/components`}>Components</Link>
-//             </li>
-//             <li>
-//                 <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-//             </li>
-//         </ul>
-//
-//         <Route path={`${match.path}/:id`} component={Topic} />
-//         <Route
-//             exact
-//             path={match.path}
-//             render={() => <h3>Please select a topic.</h3>}
-//         />
-//     </div>
+//             <Route path="/" exact component={Index} />
+//             <Route path="/about/" component={About} />
+//             <Route path="/users/" component={Users} />
+//         </div>
+//     </Router>
 // );
 
 
-
-const AppRouter = () => (
+const App2 = () => (
     <Router>
         <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about/">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/users/">Users</Link>
-                    </li>
-                </ul>
-            </nav>
+            <Header />
 
-            <Route path="/" exact component={Index} />
-            <Route path="/about/" component={About} />
-            <Route path="/users/" component={Users} />
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/topics" component={Topics} />
         </div>
     </Router>
 );
 
 
-// const App2 = () => (
-//     <Router>
-//         <div>
-//             <Header />
-//
-//             <Route exact path="/" component={Home} />
-//             <Route path="/about" component={About} />
-//             <Route path="/topics" component={Topics} />
-//         </div>
-//     </Router>
-// );
-//
-//
-// const Header = () => (
-//     <ul>
-//         <li>
-//             <Link to="/">Home</Link>
-//         </li>
-//         <li>
-//             <Link to="/about">About</Link>
-//         </li>
-//         <li>
-//             <Link to="/topics">Topics</Link>
-//         </li>
-//     </ul>
-// );
+const Header = () => (
+    <ul>
+        <li>
+            <Link to="/">Home</Link>
+        </li>
+        <li>
+            <Link to="/about">About</Link>
+        </li>
+        <li>
+            <Link to="/topics">Topics</Link>
+        </li>
+    </ul>
+);
 
 // export default AppRouter;
 // export AppRouter;
@@ -111,7 +111,7 @@ class App extends Component {
             Learn React
           </a>
 
-            <AppRouter/>
+            <App2/>
         </header>
           {/*<Header/>*/}
         {/*<App2/>*/}
