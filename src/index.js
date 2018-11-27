3333f61c40f31ad05a16b101b05926df40b71105
sultanularefin <mhmdarefin@gmail.com>
2018-11-27 18:37:34 +0600

    dsfsdrfawertwet

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 // import MouseTracker from './MouseTracker';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<MouseTracker />, document.getElementById('root'));
+// ReactDOM.render(<MouseTracker />, document.getElementById('root'));
+
+ReactDOM.render(<PureRender />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
